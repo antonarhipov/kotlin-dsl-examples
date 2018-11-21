42f5f7e0badee334c6aaafbc51e4e0feb1e98426
Anton Arhipov <ant.arhipov@gmail.com>
2018-11-22 01:20:01 +0200

    blah

--- src/main/java/org/arhan/model/Example.java
+++ src/main/java/org/arhan/model/Example.java
@@ -1,13 +1,24 @@
 package org.arhan.model;
 
-import org.arhan.DslExtensionsKt;
-import org.arhan.DslKt;
-
 public class Example {
   public static void main(String[] args) {
+    final ClientBuilder builder = new ClientBuilder();
+
+    builder.setFirstName("Anton");
+    builder.setLastName("Arhipov");
+
+    final TwitterBuilder twitterBuilder = new TwitterBuilder();
+    twitterBuilder.setHandle("@antonarhipov");
+    builder.setTwitter(twitterBuilder.build());
+
+    final CompanyBuilder companyBuilder = new CompanyBuilder();
+    companyBuilder.setName("JetBrains");
+    companyBuilder.setCity("Tallinn");
+    builder.setCompany(companyBuilder.build());
 
 
-
+    final Client client = builder.build();
+    System.out.println("Created client is: " + client);
   }
 }
 
