b1af087bed47534dd18db4c6b2937ce40aa75427
anton <ant.arhipov@gmail.com>
2021-05-06 10:38:51 +0300

    cleaninng up some code

--- src/main/kotlin/org/arhan/dsl.kt
+++ src/main/kotlin/org/arhan/dsl.kt
@@ -6,17 +6,36 @@ import org.arhan.model.CompanyBuilder
 import org.arhan.model.TwitterBuilder
 
 fun main() {
-    val client = createClient {
+    //region dsl with extensions
+    val c1 = client {
         firstName = "Anton"
         lastName = "Arhipov"
         twitter { handle = "@antonarhipov" }
         company { name = "JetBrains"; city = "Tallinn" }
         dob = 24 March 1981
     }
-    println("Created client is: $client")
+    println("Created client is: $c1")
+    //endregion
+
+    //region dsl with apply()
+    val c2 = ClientBuilder().apply {
+        firstName = "Anton"
+        lastName = "Arhipov"
+
+        twitter = TwitterBuilder().apply {
+            handle = "@antonarhipov"
+        }.build()
+
+        company = CompanyBuilder().apply {
+            name = "JetBrains"
+            city = "Tallinn"
+        }.build()
+    }.build()
+    println("Created client is: $c2")
+    //endregion
 }
 
-
+//region dsl extensions
 fun ClientBuilder.company(block: CompanyBuilder.() -> Unit) {
     company = CompanyBuilder().apply(block).build()
 }
@@ -26,10 +45,11 @@ fun ClientBuilder.twitter(block: TwitterBuilder.() -> Unit) {
 }
 
 
-fun createClient(c: ClientBuilder.() -> Unit): Client {
+fun client(c: ClientBuilder.() -> Unit): Client {
     val builder = ClientBuilder()
 
     c(builder)
 
     return builder.build()
 }
+//endregion
